7d557b4d2b5449a610116cb016f8683b461d71f3
mnansary <silentassasin0111@gmail.com>
2022-03-09 17:45:11 +0600

    reformat and clean language text

--- words.py
+++ words.py
@@ -1,28 +1,28 @@
+#-*- coding: utf-8 -*-
+"""
+@author:MD.Nazmuddoha Ansary
+"""
+from __future__ import print_function
 #---------------------------
 # imports
 #---------------------------
 import os
+import re
 from glob import glob
 import pandas as pd
 from collections import Counter
 from tqdm.auto import tqdm
 tqdm.pandas()
+import argparse
+from utils import *
+from languages import languages
 #---------------------------
 # helpers
 #---------------------------
-punctuations           =    ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', 
-                            '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`',
-                            '{', '|', '}', '~', '।', '॥','“','”'] 
-def reset(df):
-    df.dropna(inplace=True)
-    df.reset_index(drop=True,inplace=True) 
-    return df
 
-def get_words(text):
+def get_words(text,pattern):
     if text.strip():
-        for punct in punctuations+["\n"]:
-            text=text.replace(punct," ")
-        words=[word for word in text.split(" ") if word.strip()]
+        words=re.findall(pattern, text)
         return Counter(words)
     else:
         return None
@@ -39,12 +39,12 @@ def split_dataframe(df, chunk_size = 5000):
         chunks.append(df[i*chunk_size:(i+1)*chunk_size])
     return chunks
 
-def process_csv(csv,didx,words_dir):
+def process_csv(csv,didx,words_dir,pattern,chunk_size):
     df=pd.read_csv(csv)
     df=reset(df)
-    df["words"]=df.text.progress_apply(lambda x:get_words(x))
+    df["words"]=df.text.progress_apply(lambda x:get_words(x,pattern))
     df=reset(df)
-    chunks=split_dataframe(df)
+    chunks=split_dataframe(df,chunk_size=chunk_size)
     for cidx,df in enumerate(chunks):
         data=Counter()
         for idx in tqdm(range(len(df))):
@@ -53,14 +53,55 @@ def process_csv(csv,didx,words_dir):
         _csv=os.path.join(words_dir,f"{didx}_{cidx}.csv")
         data.to_csv(_csv,index=False) 
 
-if __name__=="__main__":
-    for lang in ["bn","hi","ml","gu","ta","pa","or"]:
-        data_dir=f"/backup/Oscar/data/{lang}"
-        words_dir=f"/backup/Oscar/words/{lang}/"
-        if not os.path.exists(words_dir):
-            os.mkdir(words_dir)
-            
-        csvs=[csv for csv in glob(os.path.join(data_dir,"*.csv"))]
+def main(args):
+    data_dir=args.data_dir
+    chunk_size=args.chunk_size
+    save_dir  =create_dir(data_dir,"splits")
+    stats_dir =create_dir(data_dir,"data")
+    for lang in languages.keys():
+        # unicode patterns
+        pattern  =languages[lang]
+        # lang dir
+        lang_dir =create_dir(data_dir,lang)
+        # splits dir
+        splits_dir=create_dir(save_dir,lang)
+        # process splits 
+        csvs=[csv for csv in glob(os.path.join(lang_dir,"*.csv"))]
+        LOG_INFO(f"Found {len(csvs)} csv files for {lang}")
         for didx,csv in enumerate(csvs):
-            print(lang,didx)
-            process_csv(csv,didx,words_dir)
+            LOG_INFO(f"Language:{lang} CSV:{csv}")
+            process_csv(csv,didx,splits_dir,pattern,chunk_size)
+        
+        # merge splits
+        csvs=[csv for csv in glob(os.path.join(splits_dir,"*.csv"))]
+        LOG_INFO(f"Found {len(csvs)} splits for {lang}")
+        ## read 
+        dfs=[pd.read_csv(csv) for csv in tqdm(csvs)]
+        ## concat
+        df=pd.concat(dfs,ignore_index=True)
+        df=reset(df)
+        ## group
+        df_new = df.groupby(df['word']).aggregate({"count":"sum"})
+        df_new = df_new.sort_values(by='count', ascending=False)
+        ## form stats 
+        df=pd.DataFrame({})
+        df["word"]=df_new.index.tolist()
+        df["count"]=df_new["count"].tolist()
+        df["count"]=df["count"].progress_apply(lambda x:int(x))
+        ## save
+        df.to_csv(os.path.join(stats_dir,f"{lang}.csv"),index=False)
+
+
+
+if __name__=="__main__":
+    '''
+        parsing and execution
+    '''
+    parser = argparse.ArgumentParser("Oscar Data Word conversion script")
+    parser.add_argument("data_dir", help="Path where oscar data is saved as csv: The data folder should contain sub-folders as described in readme")
+    parser.add_argument("--chunk_size",required=False,default=5000,help ="number of data to tokenize in a chunk : default=5000")
+    args = parser.parse_args()
+    main(args)
+
+            
+    
